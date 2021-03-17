58fc9bf4792c0c633ae9bb00435911834dc17b4a
Rocco <rocco.whiting@gmail.com>
2021-03-17 15:00:54 -0500

    feat: HTML and css done

--- dev-skills-lab/script.js
+++ dev-skills-lab/script.js
@@ -1,5 +1,11 @@
+const myList = [];
 let $input = $('#input');
 let $button = $('#.add-button');
 let $ul = $('#ul');
+
+$('myList').on('click', '#remove', function() {
+    $(this).closest('li').remove();
+});
+
 
  
